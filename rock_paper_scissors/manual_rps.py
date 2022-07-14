602f9f14f48b1d30575fcadfdb045898343464c1
sandybhaskar23 <108214672+sandybhaskar23@users.noreply.github.com>
2022-07-14 21:18:00 +0100

    adds comments and refines output of winner

--- rock_paper_scissors/manual_rps.py
+++ rock_paper_scissors/manual_rps.py
@@ -5,6 +5,8 @@ Create two functions: get_computer_choice and get_user_choice.
 The first function will randomly pick an option between "Rock", "Paper", and "Scissors" and return the choice.
 The second function will ask the user for an input and return it.
 
+#todo when there is a draw it only reports one over winner. Fix so it says it is a total draw.
+
 '''
 
 
@@ -14,16 +16,13 @@ class rps:
 
     def __init__(self, game_choice):
 
+        #take the dict of choices and bind it to the class
         self.game_choice = game_choice
 
-        ###assign numbers
-
+        ##get the options for the game will be used for the computer and user
         self.computer_options = list(self.game_choice.keys())
         ##swap the keys around for later to work out the winner
         self.math_choice = dict([(value, key) for key, value in self.game_choice.items()])
-        #print (self.game_choice)
-        #print (type(self.game_choice))
-        #print (self.computer_options)
 
         ##initalise the rest of code 
         self.user_choice=()
@@ -46,7 +45,7 @@ class rps:
     def get_user_choice(self):        
 
         #take in the user input
-        self.user_choice = input("Enter rock or paper or scissors ").lower()
+        self.user_choice = input("Enter rock or paper or scissors: ").lower()
         #print (self.user_choice)
         #print (self.computer_options)
         #make sure input is valid
@@ -66,7 +65,7 @@ class rps:
         Using if-elif-else statements
         Wrap the code in a function called get_winner and return the winner. This function takes two arguments: computer_choice and user_choice.
         '''        
-        print(self.math_choice)
+        
         if self.computer_chosen_rank == self.user_chosen_rank:
             ##can use either the user choice or comp choice to return
             print (f"This is a draw both you and computer played a {self.user_choice}\n")
@@ -80,6 +79,7 @@ class rps:
             # non consecutive number therefore min number is the winner
             smallest_rank = min(self.computer_chosen_rank, self.user_chosen_rank)
             print (f"Winner is {self.math_choice[smallest_rank]}\n")
+            ##increment the counter to check who the overall winner is.
             self.overall_winner[self.math_choice[smallest_rank]] =  self.overall_winner.get(self.math_choice[smallest_rank],0) +1
                
         
@@ -95,6 +95,7 @@ class rps:
 
 def play_game(game_choice):
 
+    ##call the class
     gameon = rps(game_choice)
     x =1
     while x in range(1,5):
@@ -107,10 +108,17 @@ def play_game(game_choice):
         x +=1
     
     ###Provide a support at the end of the final scores
-    print (f"The final result is")
+    print (f"#The final results are;\n")
    
-    for key,value in gameon.overall_winner.items():
-        print(f"{key} won {value}")
+    for player,value in gameon.overall_winner.items():
+        ##make it easier for the eye
+        player = player.title()
+        print(f"{player} won {value}")
+
+    ##limitation is both players have equal wins
+    overall_winner = max(gameon.overall_winner, key=gameon.overall_winner.get) 
+    print (f"The overall winner is {overall_winner}\n")
+
     print ('END')
 
 
