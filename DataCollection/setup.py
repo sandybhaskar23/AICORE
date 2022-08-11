69aea50a2965eaf86d6fc0391989afaf77455085
sandybhaskar23 <108214672+sandybhaskar23@users.noreply.github.com>
2022-08-11 21:04:50 +0100

    new adds details and dependcies for setuptools

---
+++ DataCollection/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'TP53 clinical trials '
+    version = '0.0.1',
+    description='This package extracts TP53 data consisting of genomic changes, disease and existing clinical drug trials and the phase of these trials',
+    url='https://github.com/sandybhaskar23/AICORE/tree/main/DataCollection',
+    author='Sanjeev Bhaskar',
+    license= 'MIT',
+    packages=find_packages(),
+    install_requires= ['Selenium', 'uuid' ,'time', 'urllib.request'],
+)
