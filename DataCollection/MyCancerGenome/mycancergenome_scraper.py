21cc0db4d490a4480f3d4ef659c076db06521cd7
sandybhaskar23 <108214672+sandybhaskar23@users.noreply.github.com>
2022-09-01 18:58:56 +0100

    adds sub key for clinical trials details.

--- DataCollection/MyCancerGenome/mycancergenome_scraper.py
+++ DataCollection/MyCancerGenome/mycancergenome_scraper.py
@@ -1,11 +1,15 @@
+from __future__ import unicode_literals
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
 import time
 import json
 import urllib.request 
-from MyCancerGenome  import webscraper
-
+import boto3
+import webscraper
+import pandas as  pd
 """ Creates a scraper for  TP53 the guardian of the genome.  The selenium driver reads the pages and scrolls till it captures all the card details
     Data and images are extracted to local storage as json and png
 """
@@ -27,13 +31,18 @@ class GuardianScarper:
         ##build the data
         self.summary_det={}
         self.summary_det['uuid'] = self.WS.uuid
+        ##aws handle
+        self.s3 = boto3.client('s3')
+
+        ##global defined key for dict needed for clinical trials details
+        self.cct= 'ClinicalTrialsTitle'
         
         
 
-    def get_links(self, val='small-up-1 medium-up-2 large-up-2')-> list:
+    def get_links(self, val='small-up-1 medium-up-2 large-up-2', key=None)-> list:
         """ Finds the href links in web pages
             Args:
-                driver: webdriver.Chrome
+                driver: webdriver.Edge
                 The driver that contains information about the current page
 
                 driver              : Uses contains to deal with slight variance in html class name
@@ -47,77 +56,54 @@ class GuardianScarper:
                     Stored extracted data
         """
         ##has to be class directly parent of rest of listings
-        ##need to be agnostic to nuance naming changes                               
+        ##need to be agnostic to nuance naming changes   
+        #  WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gdpr-consent-notice"]')))    
+        #WebDriverWait(self.driver.implicitly_wait(),18).until(EC.visibility_of_element_located((By.XPATH,"//div[contains(@class,'{val}')]"))) < does not work  
+        #self.driver.implicitly_wait(18) < -does not resolve issue
+        #self.driver.page_source    < does resolve issue                  
         tp53_container = self.driver.find_element(by=By.XPATH, value=f"//div[contains(@class,'{val}')]")
         #input[contains(@id,'id')]
         tp53_list = tp53_container.find_elements(by=By.XPATH, value='//div[@class="card-section"]')
+       
+        print(f'Current Url is: {self.driver.current_url}')
+        if key is not None:
+            self.summary_det[key][self.cct] = {}
 
         for tp53_property in tp53_list:
             a_tag = tp53_property.find_element(by=By.TAG_NAME, value='a')
             link = a_tag.get_attribute('href')
-       
-           
-            self.link_list.append(link)
-            ###get card details while here
-            chunks = tp53_property.text.split('\n')     
-            ##store in case I forget                       
-            self.summary_det[chunks[0]] = {'link':link}
-            ###convert rest in list into dictionary
-            _res_dct = {chunks[i].split(':')[0]: chunks[i].split(':')[-1] for i in range(1, len(chunks))}
+            print('in links')
+            print(link)
+            h5_tag = tp53_property.find_element(by=By.TAG_NAME, value='h5')
+            det = h5_tag.get_attribute('title')
             
-            self.summary_det[chunks[0]].update(_res_dct)
-            #print(self.summary_det)                
+            ###get card details while here            
+            chunks = tp53_property.text.split('\n')                 
+
+            ###convert rest in list into dictionary
+            _res_dct = {chunks[i].split(':')[0]: chunks[i].split(':')[-1]  for i in range(1, len(chunks))}
+            ##1st condition is based on sub links now being called. This will now store the sub information under the initial group key
+            if key is not None:
+                tri = type(self.summary_det[key][self.cct])
+                print(tri)
+                ##create a nested dictionary. This happens after main key has created therefore update is enough
+                self.summary_det[key][self.cct].update({ det : {'link':link}})
+                #self.summary_det[key][self.cct][det].update({'link':link})
+                self.summary_det[key][self.cct][det].update(_res_dct)                
+            else:
+                ##store infor from 1st group of links and their relevant information                     
+                self.summary_det[det] = {'link':link}
+                self.summary_det[det].update(_res_dct)
+
+            self.link_list.append(f'{det}:{link}')                           
+            
+        ##high degree of redundancy       
+        self.link_list = list(set(self.link_list))
 
         print(f'There are {len(self.link_list)} properties in this page')
         ###needs to be returned to the function calling this class method
         return self.link_list
-
-
-
-    def get_summary(self, tp53_property,card_title)-> dict:
-        """ Get summary details from each card.  
-            Each card holds variant coorindate details
-
-            Args:
-                driver              : webdriver.Chrome
-                tp53p_list (list)   : Elements of p tag from html
-                innertext (str)     : The values from elements of tp53_list and tp53p_list
-                span_tag (str)      : 1st index values of split from innertext
-                inner_text (str)    : 2nd indexed value of split from innertext
-                summary_det (dict)  : Stores the extracted data 
-
-            Returns:
-                summary_det
-                    Stored extracted data
-        """
-
-
-         ##has to be class directly parent of rest of listings
-        tp53_container = tp53_property.find_element(by=By.XPATH, value='//*[@class="news-card-article"]')
-        tp53_div = tp53_container.find_element(by=By.XPATH, value='./div')
-        tp53_list = tp53_div.find_elements(by=By.XPATH, value='./p')
-        ##tidy up naming = based on structure of tp53_container need a diff handle
-        tp53p_list = tp53_container.find_elements(by=By.XPATH, value='./p')
-        
-        for tp53_prop in tp53_list:            
-            ###get the key for this 1st div
-            _innertext = tp53_prop.text
-            ###add to dictionary
-            span_tag, inner_text = _innertext.split(':')    
-            self.summary_det[card_title] = {span_tag : inner_text }           
-        
-
-        ###need to get the p tags now            
-        for tp53p_prop in tp53p_list:
-           
-            ###get the inner text encapsulated as a react text. 
-            _innertext= tp53p_prop.text
-            ###add to dictionary
-            span_tag, inner_text = _innertext.split(':')    
-            self.summary_det[card_title] = {span_tag : inner_text }
-                 
-    
-        #time.sleep(30)   
+   
 
     def get_link_details(self, big_list):    
         """ Extracts all the details of links i.e: disease, drugs from the aggregated stored links dictionary
@@ -134,8 +120,11 @@ class GuardianScarper:
 
         
         """
-        for lnk in big_list:
-            ##now in the card details i.e TP53 nonsense ...
+        for link in big_list:
+            ##need to make sure the key is split and passed on
+            k,lnk = link.split(':',1)
+
+            ##now in the card details i.e TP53 nonsense ...            
             self.driver.get(lnk)
             ##in this case there is no images bar the logo and pathway need to traverse links to get them
             ###there is only one pathway map therefore do not store muliple times
@@ -143,10 +132,11 @@ class GuardianScarper:
             
             try:
                 clinical_trials = self.driver.find_element(by=By.PARTIAL_LINK_TEXT, value = 'View Clinical Trials')               
-                self.clintrials_link.append(clinical_trials.get_attribute('href'))    
+                self.clintrials_link.append(f"{k}:{clinical_trials.get_attribute('href')}")    
                 #print(clintrials_link)
             except:
                 ###if not present then not an issue not all variance has clinical trials
+                print('moving on')
                 continue
         ##make unique
         self.clintrials_link = list(set(self.clintrials_link))            
@@ -174,13 +164,22 @@ class GuardianScarper:
         print('clinical trials')
         #print (self.clintrials_link)
         for cln in clintrials_link:
-            #print (cln)
+            k,cln = cln.split(':',1)
+            print(k)
+            
+            print('In clinical tials details')
+            print (cln)
+            #print (self.summary_det)
+            ##need this to allow driver time to load the actual page. Misdirection technique
+            self.driver.get('https://www.mycancergenome.org/content/biomarkers/#search=TP53')
+            time.sleep(4)
             self.driver.get(cln)
+            self.driver.implicitly_wait(8)       
             self.WS.scroll_down()
             time.sleep(4)
             #print (self.driver.page_source)
             ##reuse
-            self.get_links()
+            self.get_links(key=k)
             
    
 
@@ -205,7 +204,8 @@ class GuardianScarper:
         for i, img  in enumerate(image_links):
             ##store the images but rename using index.
             fimage = (img.split('/')[-1]).replace('.png','')       
-            urllib.request.urlretrieve(img,(f"{imgdir}\{fimage}_{i}.png"))    
+            urllib.request.urlretrieve(img,(f"{imgdir}\{fimage}_{i}.png"))
+            self.s3.upload_file (f'{imgdir}\{fimage}_{i}.png' ,'mycancergenome' , f'{fimage}_{i}.png')   
         
    
     def get_path(self):
@@ -272,6 +272,11 @@ def deepmine(url="https://www.mycancergenome.org/content/biomarkers/#search=TP53
     with (p / 'data.json').open('w') as opened_file:          
           json.dump(guardscap.summary_det,opened_file)
 
+    df = pd.DataFrame(guardscap.summary_det)
+    df.to_csv(r'mycancergenome_data.csv',  mode='a')
+    print(df)
+
+    guardscap.s3.upload_file(f'{p}/data.json' ,'mycancergenome', 'data.json')
     ##download the images
     guardscap.download_images()
 
